3eb0c52fcc4760e480936ccb6226269600d92a7a
Anup <kanup3151@gmail.com>
2024-02-07 19:46:47 +0530

    trying to add signup form but i need html

--- express_backend_app/index.js
+++ express_backend_app/index.js
@@ -2,10 +2,30 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const users=[];
+
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.post('/signup', (req, res)=>{
+  const name = req.body.name;
+  const email = req.body.email;
+  const user ={
+    name,
+    email,
+  };
+  users.push(user);
+  res.send("your data added an your data is:\n");
+  console.log(users);
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+console.log(users);
